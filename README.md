b874ae596af4ecdba535c2d4159ba950f56debb7
Dom Sirotti <dsirotti@ford.com>
2022-08-18 16:47:33 -0400

    core wordos logic pretty much done

--- README.md
+++ README.md
@@ -21,11 +21,11 @@ If using DosBox or an actual DOS environment (such as MSDOS or Windows 9X, **but
 ### For FreeDOS
 If you are not able to use `FDIMPLES` or their bonus / supplemental CD then follow the instructions above for standard DOS environments. Otherwise launch `FDIMPLES` and make sure it is installed from the `DEVEL` package. You will see a script in `C:\DEVEL\WATCOM` called `OWSETENV.BAT` (or something of the sort). Running that will set all necessary environment variables so you can begin. I recommend setting this script to run on boot, or create an alias in `FDAUTO.BAT` in order to more easily run it.
 
+NOTE: The version included on `FDIMPLES` is OpenWatcomC v1.9, rather than the above linked fork OpenWatcomC v2. So far I haven't noticed any functional differences in building.
+
 ## Compile with OpenWatcomC
 I'll get a makefile eventually.
 
-`wcl -za99 -wx -za *.c`
-
-This will create `DPP.EXE`. Note: I recommend actually listing the necessary C source files rather than just doing `*.c` however going the wildcard route does technically work.
+`wcl -za99 -wx -za dpp.c wordos.c nonogram.c mathdoku.c`
 
 Also worth noting is that the `-za99` flag enables the currently experimental and unfinished `C99` support within the compiler. It does enable many of C99's features however it does lack many as well - most notably variable length arrays. I'm using it for now because it is what I am used to however at some point I may just switch to full C89/C90.
--- src/wordos.c
+++ src/wordos.c
@@ -6,11 +6,64 @@
 #include <conio.h>
 #include <string.h>
 
+#define DEBUG       1
+
 #define WORD_LEN    5
+#define GUESSES     6
 
 #define N_MATCH     0
 #define P_MATCH     6
 #define F_MATCH     2
+
+const char keys[26] = {
+    'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
+    'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L',
+    'Z', 'X', 'C', 'V', 'B', 'N', 'M' 
+};
+
+typedef struct kbd {
+    char keys[26];
+    int status[26];
+} kbd_t;
+
+typedef struct ltr_freq {
+    char letter;
+    int freq;
+} ltr_freq_t;
+
+void init_kbd(kbd_t *kbd) {
+    for (int k = 0; k < 26; k++) {
+        kbd->keys[k] = keys[k];
+        kbd->status[k] = 1;
+    }
+}
+
+void set_kbd(kbd_t *kbd, char c, int status) {
+    for (int k = 0; k < 26; k++) {
+        if (kbd->keys[k] == c) {
+            if (kbd->status[k] != F_MATCH) {
+                kbd->status[k] = status;
+            }
+        }
+    }
+}
+
+void init_freqs(ltr_freq_t *freqs) {
+    for (int i = 0; i < WORD_LEN; i++) {
+        freqs[i].letter = '\0';
+        freqs[i].freq = 0;
+    }
+}
+
+int freq_contains(ltr_freq_t *freqs, char letter) {
+    for (int i = 0; i < WORD_LEN; i++) {
+        if (freqs[i].letter == letter) {
+            return i;
+        }
+    }
+
+    return -1;
+}
 
 int count_lines(FILE *fp) {
     int c;
@@ -98,6 +151,25 @@ void title_bar(char *text) {
     _outtext(text);
 }
 
+void print_keyboard(kbd_t *kbd) {
+
+    int offset_y = 0;
+    int offset_x;
+    for (int k = 0; k < 26; k++) {
+        offset_x = (k % 6) * 4;
+        _settextwindow(3 + (offset_y * 4), 55 + offset_x, 5 + (offset_y * 4), 57 + offset_x);
+        _setbkcolor(kbd->status[k]);
+        _clearscreen(_GWINDOW);
+        _settextcolor(15);
+        _settextposition(2, 2);
+        putch(kbd->keys[k]);
+
+        if ((57 + (offset_x + 4)) > 79) {
+            offset_y++;
+        }
+    }
+}
+
 void print_guess(int *test_colors, char *guess, int guess_num) {
 
     // char title[30];
@@ -114,29 +186,75 @@ void print_guess(int *test_colors, char *guess, int guess_num) {
     }
 }
 
-void compare_guess(int *result, char *guess, char *word) {
-    // result[0] = N_MATCH;
-    // result[1] = P_MATCH;
-    // result[2] = F_MATCH;
-    // result[3] = N_MATCH;
-    // result[4] = F_MATCH;
+void log_message(char *msg, int count) {
+    if (DEBUG) {
+        if (count == 0) {
+            _settextwindow(9, 2, 23, 25);
+            _setbkcolor(4);
+            _clearscreen(_GWINDOW);
+            _settextcolor(15);
+            _settextposition(2, 5);
+            _outtext("Debug Messaging:");
+        }
+
+        _settextposition(4 + count, 2);
+        _outtext(msg);
+    }
+}
+
+void compare_guess(int *result, char *guess, char *word, ltr_freq_t *freqs, kbd_t *kbd) {
 
     int lc;
+    int debug = 0;
+    char msg[25];
+    int contains = 0;
     for (int gl = 0; gl < WORD_LEN; gl++) {
-        printf("comparing %c to %c\n", guess[gl], word[gl]);
+
+        snprintf(msg, 25, "compare %c with %c at %d", guess[gl], word[gl], gl);
+        log_message(msg, debug++);
         /* Check for green */
         if (guess[gl] == word[gl]) {
             result[gl] = F_MATCH;
-            printf("\tposition %d was a match\n", gl);
+
+            contains = freq_contains(freqs, guess[gl]);
+            freqs[contains].freq--;
+
+            /* Fix erroneous yellows e.g. SOGGY -> YYYYY = YBBBG instead of BBBBG */
+            if (freqs[contains].freq < 0) {
+                for (int rw = gl - 1; gl >= 0; rw--) {
+                    if ((guess[rw] == word[gl]) && (result[rw] == P_MATCH)) {
+                        result[rw] = N_MATCH;
+                        break;
+                    }
+                }
+            }
+
+            set_kbd(kbd, guess[gl], F_MATCH);
+
+            snprintf(msg, 25, ">found a match!");
+            log_message(msg, debug++);
         } else {
             /* Check for yellow */
             lc = 0;
             for (int wl = 0; wl < WORD_LEN; wl++) {
                 if (guess[gl] == word[wl]) {
-                    result[gl] = P_MATCH;
-                    lc++;
-                    printf("\tposition %d is yellow matching word %d\n", gl, wl);
-                    break;
+                    contains = freq_contains(freqs, guess[gl]);
+
+                    if (freqs[contains].freq > 0) {
+                        freqs[contains].freq--;
+                        result[gl] = P_MATCH;
+                        set_kbd(kbd, guess[gl], P_MATCH);
+                        lc++;
+                        snprintf(msg, 25, ">partial match lc=%d", lc);
+                        log_message(msg, debug++);
+                        break;
+                    }  else {
+                        result[gl] = N_MATCH;
+                        set_kbd(kbd, guess[gl], N_MATCH);
+                        snprintf(msg, 25, ">freq limit hit");
+                        log_message(msg, debug++);
+                        break;
+                    }
                 }
             }
 
@@ -152,12 +270,15 @@ void compare_guess(int *result, char *guess, char *word) {
                     FLOGS = BBYGY
                     GROGS = YBYGY
                     GROGG = YBYGB
+                    FLOSS = BBYYB
             */
 
             /* Mark as black */
             if (lc == 0) {
-                printf("\tno bueno\n");
+                snprintf(msg, 25, ">no bueno");
+                log_message(msg, debug++);
                 result[gl] = N_MATCH;
+                set_kbd(kbd, guess[gl], N_MATCH);
             }
         }
     }
@@ -173,18 +294,37 @@ void play(char *word) {
     title_bar(title);
     // title_bar("WORDOS: Solve for the Secret Word!");
 
+    ltr_freq_t freqs[WORD_LEN];
+    init_freqs(&freqs);
+    int f_count = 0;
+    int contains;
+    for (int l = 0; l < WORD_LEN; l++) {
+        contains = freq_contains(&freqs, word[l]);
+        if (contains == -1) {
+            freqs[f_count].letter = word[l];
+            freqs[f_count].freq++;
+            f_count++;
+        } else {
+            freqs[contains].freq++;
+        }
+    }
+
     char guess[WORD_LEN + 1];
     int cmp[WORD_LEN];
-    for (int g = 0; g < 6; g++) {
+    kbd_t kbd;
+    init_kbd(&kbd);
+    for (int g = 0; g < GUESSES; g++) {
+        print_keyboard(&kbd);
         enter_guess(&guess);
-        compare_guess(&cmp, &guess, word);
 
-        // _settextposition(5, 13);
-        // _settextcolor(3);
-        // _outtext(guess);
+        ltr_freq_t temp[WORD_LEN];
+        init_freqs(&temp);
+        memcpy(&temp, &freqs, sizeof freqs);
 
+        compare_guess(&cmp, &guess, word, &temp, &kbd);
         print_guess(&cmp, guess, g);
     }
+    print_keyboard(&kbd);
 
     getch();
 }
